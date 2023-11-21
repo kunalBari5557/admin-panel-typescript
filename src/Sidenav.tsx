355dd38e4d6271c44297f8ec6dd6c0b592a9bdbb
kunal <kunalaage21@gmail.com>
2023-11-21 17:08:37 +0530

    features: full screen

--- src/Sidenav.tsx
+++ src/Sidenav.tsx
@@ -27,6 +27,8 @@ import AddUser from "./components/Users/AddUser";
 import ViewUserDetails from "./components/Users/ViewUserDetails";
 import EditUser from "./components/Users/EditUser";
 import Tooltip from "@mui/material/Tooltip";
+import { BsArrowsFullscreen, BsFullscreen } from "react-icons/bs";
+import { BsFullscreenExit } from "react-icons/bs";
 
 const drawerWidth = 240;
 
@@ -80,18 +82,9 @@ const Drawer = styled(MuiDrawer, {
     ...openedMixin(theme),
     "& .MuiDrawer-paper": openedMixin(theme),
   }),
-  // ...(!open && {
-  //   ...closedMixin(theme),
-  //   "& .MuiDrawer-paper": closedMixin(theme),
-  // }),
   ...(!open && {
     ...closedMixin(theme),
     "& .MuiDrawer-paper": closedMixin(theme),
-    "& .responsive-text": {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    },
   }),
 }));
 
@@ -99,6 +92,37 @@ export default function Sidenav() {
   const [open, setOpen] = useState(true);
   const location = useLocation();
   const tokenState = localStorage.getItem("Token");
+
+  const [isFullScreen, setIsFullScreen] = useState<any>(false);
+
+  const handleFullscreen = () => {
+    const element: any = document.documentElement; // Use document.documentElement for fullscreen on the entire document
+    if (!document.fullscreenElement) {
+      // Enter fullscreen
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if ((element as any).mozRequestFullScreen) {
+        (element as any).mozRequestFullScreen(); // Type assertion
+      } else if ((element as any).webkitRequestFullscreen) {
+        (element as any).webkitRequestFullscreen(); // Type assertion
+      } else if ((element as any).msRequestFullscreen) {
+        (element as any).msRequestFullscreen(); // Type assertion
+      }
+      setIsFullScreen(true);
+    } else {
+      // Exit fullscreen
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if ((document as any).mozCancelFullScreen) {
+        (document as any).mozCancelFullScreen(); // Type assertion
+      } else if ((document as any).webkitExitFullscreen) {
+        (document as any).webkitExitFullscreen(); // Type assertion
+      } else if ((document as any).msExitFullscreen) {
+        (document as any).msExitFullscreen(); // Type assertion
+      }
+      setIsFullScreen(false);
+    }
+  };
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -121,7 +145,41 @@ export default function Sidenav() {
             <Typography variant="h6" noWrap component="div">
               <img src="./youtube.png" height={40} alt="YouTube Logo" />
             </Typography>
-            <LogoutButton />
+            <div
+              style={{
+                display: "flex",
+                alignItems: "flex-end",
+                marginLeft: "auto",
+                cursor: "pointer",
+              }}
+            >
+              {isFullScreen ? (
+                <BsFullscreenExit
+                  style={{
+                    color: "black",
+                    height: "25px",
+                    marginRight: "1rem",
+                    marginBottom: "0.2rem",
+                    width: "25px",
+                    fontWeight: "bold",
+                  }}
+                  onClick={handleFullscreen}
+                />
+              ) : (
+                <BsFullscreen
+                  style={{
+                    color: "black",
+                    height: "25px",
+                    marginRight: "2rem",
+                    marginBottom: "0.2rem",
+                    width: "25px",
+                    fontWeight: "bold",
+                  }}
+                  onClick={handleFullscreen}
+                />
+              )}
+              <LogoutButton />
+            </div>
           </Toolbar>
         </AppBar>
       )}
@@ -159,9 +217,7 @@ export default function Sidenav() {
               <ListItemButton
                 style={{
                   backgroundColor:
-                    location.pathname === "/Products"
-                      ? "black"
-                      : "transparent",
+                    location.pathname === "/Products" ? "black" : "transparent",
                 }}
               >
                 <Tooltip title="Products" arrow placement="right">
